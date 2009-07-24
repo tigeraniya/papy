24096cf40738b6b8cb006b4fe9e6dc1e2c4f5496
marcin.cieslik <marcin.cieslik@fdd8c0ba-47b3-11de-ba0e-b5da6ac7b600>
2009-07-24 16:08:57 +0000

    test_papy remodelling

--- trunk/test/dummy.py
+++
@@ -1,8 +0,0 @@
-from IMap import *
-
-@imports([['os',[]], ['socket',[]], ['sys', []]])
-def hid(i):
-    caller = "%s" % repr(sys._getframe(1).f_locals['self'])
-    return "%s is on host:%s, process:%s, using hid:%s, calledby:%s(%s)" %\
-    (i[0], socket.gethostname(), os.getpid(), id(hid), caller, 1)
-
